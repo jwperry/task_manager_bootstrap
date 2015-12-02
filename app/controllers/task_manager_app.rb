0f4b3992abe9f25d206194c7bbd14fac640a7d2c
Joe Perry <joseph.w.perry@gmail.com>
2015-12-01 21:23:08 -0700

    Adding CRUD support

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,27 +2,47 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
-  get '/' do
+  get "/" do
     erb :dashboard
   end
 
-  get '/tasks' do
+  get "/tasks" do
     @tasks = TaskManager.all
     erb :index
   end
 
-  get '/tasks/new' do
+  get "/tasks/new" do
     erb :new
   end
 
-  post '/tasks' do
+  post "/tasks" do
     TaskManager.create(params[:task])
-    redirect '/tasks'
+    redirect "/tasks"
   end
 
-  get '/tasks/:id' do |id|
+  get "/tasks/:id" do |id|
     @task = TaskManager.find(id.to_i)
     erb :show
+  end
+
+  not_found do
+    erb :error
+  end
+
+  get "/tasks/:id/edit" do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :edit
+  end
+
+  put "/tasks/:id" do |id|
+    TaskManager.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete "/tasks/:id" do |id|
+    TaskManager.delete(id.to_i)
+    redirect "/tasks"
   end
 end
